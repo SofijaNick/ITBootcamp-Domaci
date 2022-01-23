2ebd2fcfa2c75d938e1df4df3582bffe98f7fa87
SofijaNick <sofijaa.nevo@gmail.com>
2022-01-23 17:15:36 +0100

    Domaci 13

---
+++ D_13_Sofija_Nikolic_3416/script.js
@@ -0,0 +1,46 @@
+getSportisti = (resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200){
+            let data = JSON.parse(request.responseText)
+            resolve(data);
+        }else if(request.readyState === 4){
+            reject("Could not fetch the data");
+        }
+    });
+    request.open('GET', 'sportisti.json')
+    request.send();
+}
+
+let visina = niz => {
+    let zbir = 0;
+    let broj = 0;
+    niz.forEach(s => {
+        zbir += parseInt(s.visina);
+        broj++
+    })
+    console.log(zbir / broj)
+}
+getSportisti(visina)
+
+let transferi = niz => {
+    let najmanje = niz[0];
+    niz.forEach(s => {
+        if(s.timovi < najmanje.timovi){
+            najmanje = s;
+        }
+    })
+    console.log(najmanje.imePrezime)
+}
+getSportisti(transferi);
+
+let zaLakerse = niz => {
+    niz.forEach(s => {
+        if(s.timovi.includes("Lakers")){
+            console.log(s.imePrezime)
+        }
+    })
+}
+
+getSportisti(zaLakerse)
+
